8f6a0e1568d77b9d7c8596a2d033dd16a3f6a117
Анна Михайлец <amile@amile-mbp.kvm-router>
2017-08-15 20:23:51 +0300

    Add data-header, data-main

--- app/blocks/header/header.js
+++ app/blocks/header/header.js
@@ -41,6 +41,7 @@ function changeSelectedValue(event) {
 		$(el).off();
 	});
 } */
+
 function changeSelectedValue(event) {
 	event.stopPropagation();
 	const currentValue = $(this).text();
@@ -59,3 +60,16 @@ $('.currency-selector').click(function () {
 	$('.currency-selector__value').on('click', changeSelectedValue);
 });
 
+function hideMenu() {
+	console.log('I"m clicked');
+	$('.main-menu__menu').slideUp(600);
+	$('.main-menu').off('click', hideMenu);
+}
+
+function showMenu() {
+	console.log('I"m button');
+	$('.main-menu__menu').slideDown(600);
+	$('.main-menu').on('click', hideMenu);
+}
+
+$('.main-menu').on('click', showMenu);
