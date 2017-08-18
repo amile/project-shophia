fb2b267de29c5e9a08e9dd0b227964b593bbe1dd
Анна Михайлец <amile@amile-mbp.kvm-router>
2017-08-18 13:40:21 +0300

    Convert support items to json

--- app/blocks/header/header.js
+++ app/blocks/header/header.js
@@ -15,33 +15,6 @@ import $ from 'jquery';
 
 const valueList = $('.currency-selector__value');
 
-/* work variant
-
-$('.currency-selector').click(function () {
-	Array.prototype.forEach.call(valueList, function (el) {
-		$(el).addClass('currency-selector__value_selected');
-	});
-	Array.prototype.forEach.call(valueList, function (el) {
-		$(el).click(changeSelectedValue);
-	});
-});
-function changeSelectedValue(event) {
-	event.stopPropagation();
-	const currentValue = $(this).text();
-	const lastValue = $(valueList[0]).text();
-	$(this).text(lastValue);
-	$(valueList[0]).text(currentValue);
-	Array.prototype.forEach.call(valueList, function (el) {
-		console.log('none');
-		if (el !== valueList[0]) {
-			$(el).removeClass('currency-selector__value_selected');
-		}
-	});
-	Array.prototype.forEach.call(valueList, function (el) {
-		$(el).off();
-	});
-} */
-
 function changeSelectedValue(event) {
 	event.stopPropagation();
 	const currentValue = $(this).text();
