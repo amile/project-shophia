59bc5a08f5770ccdab3bb5ecf4799348c231a0ae
Анна Михайлец <amile@amile-mbp.kvm-router>
2017-09-18 16:17:18 +0300

    Change variables in stylus

--- app/blocks/header/header.js
+++ app/blocks/header/header.js
@@ -1,15 +1,3 @@
-/* const currencySelector = document.getElementsByClassName('currency-selector__selector')[0];
-
-const selectorOuptut = document.getElementsByClassName('currency-selector__value')[0];
-
-
-
-currencySelector.addEventListener('change', function () {
-
-	const selectedValue = currencySelector.options[currencySelector.selectedIndex].value;
-
-	selectorOuptut.innerHTML = selectedValue;
-}); */
 
 import $ from 'jquery';
 
